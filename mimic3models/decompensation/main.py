2b12ce4b605beb42279013f971ee58627f7aa2fa
islamazhar <mazhar.buet11@gmail.com>
2020-01-23 14:15:13 +0000

    changes

--- mimic3models/decompensation/main.py
+++ mimic3models/decompensation/main.py
@@ -6,7 +6,6 @@ import argparse
 import os
 import imp
 import re
-import copy
 
 from mimic3models.decompensation import utils
 from mimic3benchmark.readers import DecompensationReader
@@ -18,11 +17,7 @@ from mimic3models import common_utils
 
 from keras.callbacks import ModelCheckpoint, CSVLogger
 
-# deep supervision
-# dropuout rate
-# Bidirectional_
-# BatchGenDeepSupervision
-# form the Discretizer header they cont_channels and those continued channels are going to be normalize..
+
 parser = argparse.ArgumentParser()
 common_utils.add_common_arguments(parser)
 parser.add_argument('--deep_supervision', dest='deep_supervision', action='store_true')
@@ -34,8 +29,8 @@ parser.set_defaults(deep_supervision=False)
 args = parser.parse_args()
 print(args)
 
-if args.small_part:
-    args.save_every = 2**30
+#if args.small_part:
+#    args.save_every = 2**30
 
 # Build readers, discretizers, normalizers
 if args.deep_supervision:
@@ -55,8 +50,7 @@ discretizer = Discretizer(timestep=args.timestep,
                           store_masks=True,
                           impute_strategy='previous',
                           start_time='zero')
-# Mazhar: discretizer_header is used in args and also to set cont_channels --> normalizer...
-# Mazhar: if normalizer is None then use the previosuly save file?
+
 if args.deep_supervision:
     discretizer_header = discretizer.transform(train_data_loader._data["X"][0])[1].split(',')
 else:
@@ -68,18 +62,17 @@ normalizer_state = args.normalizer_state
 if normalizer_state is None:
     normalizer_state = 'decomp_ts{}.input_str:previous.n1e5.start_time:zero.normalizer'.format(args.timestep)
     normalizer_state = os.path.join(os.path.dirname(__file__), normalizer_state)
-normalizer.load_params(normalizer_state) # where the normalizer is used?
+normalizer.load_params(normalizer_state)
 
-args_dict = dict(args._get_kwargs()) # Mazhar: getting all the command line arguments...
-args_dict['header'] = discretizer_header # Mazhar:  `discretizer_header` is taken from train_data_loader (if deep supervision is on) otherwise from train_reader.
+args_dict = dict(args._get_kwargs())
+args_dict['header'] = discretizer_header
 args_dict['task'] = 'decomp'
 
 
 # Build the model
 print("==> using model {}".format(args.network))
-model_module = imp.load_source(os.path.basename(args.network), args.network) # loading the args.network module dynamically...what is the value of args.network?
+model_module = imp.load_source(os.path.basename(args.network), args.network)
 model = model_module.Network(**args_dict)
-# dim, batch_norm, dropout, rec_dropout, task, target_repl=False, deep_supervision=False, num_classes=1, depth=1, input_dim=76, **kwargs
 suffix = "{}.bs{}{}{}.ts{}".format("" if not args.deep_supervision else ".dsup",
                                    args.batch_size,
                                    ".L1{}".format(args.l1) if args.l1 > 0 else "",
@@ -87,25 +80,24 @@ suffix = "{}.bs{}{}{}.ts{}".format("" if not args.deep_supervision else ".dsup",
                                    args.timestep)
 model.final_name = args.prefix + model.say_name() + suffix
 print("==> model.final_name:", model.final_name)
-#> model.final_name: k_lstm.n128.dep1.bs8.ts1.0
+
 
 # Compile the model
 print("==> compiling the model")
 optimizer_config = {'class_name': args.optimizer,
                     'config': {'lr': args.lr,
                                'beta_1': args.beta_1}}
-# Adam is an adaptive learning rate (optimizer...)
 
 # NOTE: one can use binary_crossentropy even for (B, T, C) shape.
 #       It will calculate binary_crossentropies for each class
 #       and then take the mean over axis=-1. Tre results is (B, T).
 model.compile(optimizer=optimizer_config,
-              loss='binary_crossentropy') # standard lstm code
+              loss='binary_crossentropy')
 model.summary()
-#namespace(batch_norm=False, batch_size=8, beta_1=0.9, data='/home/danfeng/project/mimic3-benchmarks/mimic3models/decompensation/../../data/decompensation/', deep_supervision=False, depth=1, dim=128, dropout=0.0, epochs=100, imputation='previous', l1=0, l2=0, load_state='', lr=0.001, mode='train', network='mimic3models/keras_models/lstm.py', normalizer_state=None, optimizer='adam', output_dir='mimic3models/decompensation', prefix='', rec_dropout=0.0, save_every=1, size_coef=4.0, small_part=False, timestep=1.0, verbose=2)
+
 # Load model weights
-n_trained_chunks = 0 # what?
-if args.load_state != "": # what is load_state?
+n_trained_chunks = 0
+if args.load_state != "":
     model.load_weights(args.load_state)
     n_trained_chunks = int(re.match(".*chunk([0-9]+).*", args.load_state).group(1))
 
@@ -118,29 +110,37 @@ if args.deep_supervision:
 else:
     # Set number of batches in one epoch
     train_nbatches = 2000
-    val_nbatches = 1000 # Why 1000
+    val_nbatches = 1000
     if args.small_part:
         train_nbatches = 40
         val_nbatches = 40
     train_data_gen = utils.BatchGen(train_reader, discretizer,
-                                    normalizer, args.batch_size, train_nbatches, True, 1, 1)
+                                    normalizer, args.batch_size, train_nbatches, False, False, 1, 1)
     val_data_gen = utils.BatchGen(val_reader, discretizer,
-                                  normalizer, args.batch_size, val_nbatches, False)
-# print(type(train_data_gen))
-
-class_1 = 0
-class_0 = 0
+                                  normalizer, args.batch_size, val_nbatches, False, False, 1, 1)
+"""
+class_0 = 0.0
+class_1 = 0.0
+print("calculating frequency")
 for i in range(train_data_gen.steps):
-    ret = next(train_data_gen)
-    for c in ret[1]:
-        if c == 0: class_0 += 1
-        else: class_1 += 1
-print("Number of class 1 instances= ", class_1, "Number of class 0 instances= ", class_0)
-# calculating frequency
-# class 0 =  6.382435537401073e-05 | class 1  =  0.0029940119760479044
-train_reader = DecompensationReader(dataset_dir=os.path.join(args.data, 'train'),listfile=os.path.join(args.data, 'train_listfile.csv'))
-train_data_gen = utils.BatchGen(train_reader, discretizer, normalizer, args.batch_size, train_nbatches, True, 1/class_1, 1/class_0)
+	ret = next(train_data_gen)
+	#print(ret)
+	for c in ret[1]:
+		if c == 0: class_0 += 1
+		else: class_1 += 1
 
+c0 = class_1/(class_0+class_1)
+c1 = class_0/(class_1+class_0)
+#c0 = 0.015
+#c1 = 1
+
+print("class 0 ", class_0, "class 1 ", class_1, "c0", c0, "c1", c1)
+
+train_reader = DecompensationReader(dataset_dir=os.path.join(args.data, 'train'),listfile=os.path.join(args.data, 'train_listfile.csv'))
+train_data_gen = utils.BatchGen(train_reader, discretizer, normalizer, args.batch_size, train_nbatches, False, False, c0, c1)
+val_data_gen = utils.BatchGen(val_reader, discretizer,  normalizer, args.batch_size, train_nbatches, False, False, c0, c1)
+
+"""                                    
 if args.mode == 'train':
 
     # Prepare training
@@ -171,7 +171,7 @@ if args.mode == 'train':
                         epochs=n_trained_chunks + args.epochs,
                         initial_epoch=n_trained_chunks,
                         callbacks=[metrics_callback, saver, csv_logger],
-                        verbose=args.verbose) # the actual params... # https://www.geeksforgeeks.org/keras-fit-and-keras-fit_generator/
+                        verbose=args.verbose)
 
 elif args.mode == 'test':
 
@@ -195,7 +195,7 @@ elif args.mode == 'test':
                                                       shuffle=False, return_names=True)
 
         for i in range(test_data_gen.steps):
-            print("\tdone {}/{}".format(i, test_data_gen.steps), end='\r') # Done? should it be predicting?
+            print("\tdone {}/{}".format(i, test_data_gen.steps), end='\r')
             ret = next(test_data_gen)
             (x, y) = ret["data"]
             cur_names = np.array(ret["names"]).repeat(x[0].shape[1], axis=-1)
@@ -210,28 +210,26 @@ elif args.mode == 'test':
                     predictions.append(p)
                     names.append(name)
         print('\n')
-    else: # what if small_part is On now??
+    else:
         del train_reader
         del val_reader
         test_reader = DecompensationReader(dataset_dir=os.path.join(args.data, 'test'),
                                            listfile=os.path.join(args.data, 'test_listfile.csv'))
-        if args.small_part:
-            test_nbatches = 40
-            #val_nbatches = 40
-
+        test_nbatches = None
+        if args.small_part: test_nbatches = 1024
         test_data_gen = utils.BatchGen(test_reader, discretizer,
-                                       normalizer, args.batch_size, test_nbatches
-                                       ,shuffle=False, return_names=True)  # put steps = None for a full test
+                                       normalizer, args.batch_size,
+                                       test_nbatches, shuffle=False, return_names=True)  # put steps = None for a full test
 
         for i in range(test_data_gen.steps):
             print("predicting {} / {}".format(i, test_data_gen.steps), end='\r')
             ret = next(test_data_gen)
-            x, y = ret["data"]
+            x, y= ret["data"]
             cur_names = ret["names"]
             cur_ts = ret["ts"]
 
             x = np.array(x)
-            pred = model.predict_on_batch(x)[:, 0] # when deep supervision is ON it is model.predict(x, batch_size=args.batch_size)
+            pred = model.predict_on_batch(x)[:, 0]
             predictions += list(pred)
             labels += list(y)
             names += list(cur_names)
